f2f0149b49bcd25cfea5b966282d650846d1fdfe
Ashish Mishra <akmishra30@hotmail.com>
2018-12-31 20:59:09 +0800

    Added Flutter Drawer Demo.

---
+++ flutter_drawer/lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Drawer Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Drawer Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Color.fromRGBO(50, 60, 80, 1.0),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("This drawer example.", style: TextStyle(color: Colors.black, fontSize: 30))
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        elevation: 20,
+        child: ListView(
+          padding: EdgeInsets.only(bottom: 5.0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('Ashish Mishra', style: TextStyle(fontWeight: FontWeight.bold),),
+              accountEmail: Text('akmishra30@hotmail.com'),
+              currentAccountPicture: Image.asset('images/profile.jpg'),
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(10.0)
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo),
+              title: Text('Photos'),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+            ),
+            Divider(
+              height: 2.0,
+            ),
+            ListTile(
+              leading: Icon(Icons.video_library),
+              title: Text('Videos'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+            ),
+            Divider(
+              height: 2.0,
+            ),
+            ListTile(
+              leading: Icon(Icons.audiotrack),
+              title: Text('Audios'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            Divider(
+              height: 2.0,
+            ),
+            ListTile(
+              leading: Icon(Icons.attachment),
+              title: Text('Documents'),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+            ),
+            Divider(
+              height: 2.0,
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+                onTap: () {
+                  Navigator.pop(context);
+                }
+            ),
+            Divider(
+              height: 2.0,
+            ),
+          ]
+        ),
+      ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
